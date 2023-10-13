d69f90976cdd60b508a890bd47173ce628ed64e6
Eric Zhang <eric.zhang840@gmail.com>
2023-10-14 07:35:47 +0800

    Revert "If the input is not valid (float-convertible) then return False, and the error."
    This reverts commit 6ff2076fe6638a628735adf3c6021c6dec1e83ea.

--- main.py
+++ main.py
@@ -43,5 +43,3 @@ def main_input():
          error = f"Error: Invalid y coordinate for vertex {name}, expected float, got {ycoord}"
       else:
          validity = True
-      if not validity:
-            return False, error
