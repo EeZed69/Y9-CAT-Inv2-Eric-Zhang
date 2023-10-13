0f15e0e27d101ed36c419d605ae36a228b736adc
Eric Zhang <eric.zhang840@gmail.com>
2023-10-13 11:31:29 +0800

    Added marking key to the repository folder Added if statements that check if the first value of the result variable is true or not

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ def get_input(message: str, handler, context=None):
      while True:
         inp = input(message)
         result = handler(inp, context)
+        if result[0]:
+           return result[1]
+        elif result[0] == False:
+           print(result[1])
+           
